3527b7adebbe3bdd2fb0fbf269a76ec63f74a927
lucyck255 <122390454+lucyck255@users.noreply.github.com>
2023-02-01 11:15:21 +0000

    adding javascript test

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,1 @@
-const timeElement = document.querySelector (".time");
-const timeElement = document.querySelector (".date");
-
-/**
- * 
- * @param {Date} date 
- */
-function formatTime(date) {
-  const hours12  = date.getHours() % 12 || 12;
-  const minutes = date.getMinutes();
- 
-  return `${hours12.toString}:${minutes}`;
-
-}
+console.log("This is a JavaScript test");
